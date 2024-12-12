75230f110ed613c4d46157e9edbb3dd2a4c1de7a
chauluu <chau96cc@gmail.com>
2024-12-13 05:37:35 +0700

    chore: set private mongo

--- api/libs/db.ts
+++ api/libs/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false)
 
-const mongoDB = "mongodb+srv://chau96cc:bubtjH3YwlxKEAGG@cluster0.o9pfd.mongodb.net/Utube?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = `mongodb+srv://chau96cc:${process.env.PRIVATEMONGO}@cluster0.o9pfd.mongodb.net/Utube?retryWrites=true&w=majority&appName=Cluster0`
 
 export async function main() {
     await mongoose.connect(mongoDB)
