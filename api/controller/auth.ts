f1ce29f763afcb918f63d9108b4caae5e84ebad8
chauluu <chau96cc@gmail.com>
2024-12-13 09:29:53 +0700

    chore: test oauth

--- api/controller/auth.ts
+++ api/controller/auth.ts
@@ -12,7 +12,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            callbackURL: '/auth/google/callback',
+            callbackURL: 'https://tech-diary-backend.vercel.app/auth/google/callback',
         },
         async (accessToken: string,
             refreshToken: string,
